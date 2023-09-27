c7d154b80d5c7a2870d3b53f3b79af819dc572aa
Arianna Mireles <arianna.mireles02@gmail.com>
2023-09-26 20:38:31 -0500

    add an assets folder

---
+++ index.js
@@ -0,0 +1,5 @@
+// create a score system (if asnwer is true then add 1 point to the total sscore)
+
+// have character involvement (every level moves the character through the map till they reach the end markets to more children rather than a trivia game for adults)
+
+// 
